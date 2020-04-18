ab54fbbbc09bbf36085b2dd7beae488287c01077
Unknown <titwin@live.fr>
2020-04-18 12:26:14 +0200

    Player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector3 direction;
+    public float directionSpeed = 0.7f;
+    public float carSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 d = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        direction = Vector3.MoveTowards(direction, d, directionSpeed * Time.deltaTime);
+        transform.right = direction;
+        transform.position += direction * carSpeed * Time.deltaTime;
+    }
+}
