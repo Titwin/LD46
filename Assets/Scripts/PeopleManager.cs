4bb1c3f47d9a5353037ba3772412642e50536c20
Sol Roo <solroo@gmail.com>
2020-04-18 13:11:23 +0200

    people

---
+++ Assets/Scripts/PeopleManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleManager : MonoBehaviour
+{
+    public List<Person> people;
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Person p in people)
+        {
+            p.Think();
+            p.Act();
+        }
+    }
+}
--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -7,15 +7,56 @@ public class Person : MonoBehaviour
     public int hp = 1;
     public bool alive = true;
 
+    public Vector2 Direction = Vector2.left;
     public Rigidbody2D rb;
     public Collider2D collider;
     public SpriteRenderer renderer;
+    float t = 0;
     private void OnValidate()
     {
         rb = GetComponent<Rigidbody2D>();
         collider = GetComponent<Collider2D>();
         renderer = GetComponent<SpriteRenderer>();
     }
+
+    public void Think()
+    {
+        if (!alive)
+        {
+            return;
+        }
+        t += Time.deltaTime;
+        if (t > 1)
+        {
+            t -= 1;
+            float r = Random.value;
+            if (r < 1 / 4f)
+            {
+                Direction = new Vector2(-1, 0);
+            }
+            else if (r < 2 / 4f)
+            {
+                Direction = new Vector2(1, 0);
+            }
+            else if (r < 3 / 4f)
+            {
+                Direction = new Vector2(0,-1);
+            }
+            else if (r < 4 / 4f)
+            {
+                Direction = new Vector2(0, 1);
+            }
+        }
+    }
+    public void Act()
+    {
+        if (!alive)
+        {
+            return;
+        }
+        this.rb.MovePosition(this.rb.position + Direction * Time.deltaTime);
+    }
+
     public void Hurt()
     {
         if (alive)
