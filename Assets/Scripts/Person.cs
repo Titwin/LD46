8d906d8287b4af18fce9a839b65f0b518342f34b
Sol Roo <solroo@gmail.com>
2020-04-18 12:27:17 +0200

    base empty classes for workflow

---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    public int hp = 1;
+    public bool alive = true;
+
+    public Rigidbody2D rb;
+    public Collider2D collider;
+    public SpriteRenderer renderer;
+    private void OnValidate()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        renderer = GetComponent<SpriteRenderer>();
+    }
+    public void Hurt()
+    {
+        if (alive)
+        {
+            --hp;
+            if (hp <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    void Die()
+    {
+        //rb.simulated = false;
+        renderer.color = Color.red;
+        alive = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == Constants.LayerCar)
+        {
+            Hurt();
+        }
+    }
+}
