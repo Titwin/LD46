fa58ce9ca51124439dc9e1f2676fb2a720601df1
Unknown <titwin@live.fr>
2020-04-19 16:39:37 +0200

    finish car AI merge

---
+++ Assets/Sprites/CarLooking.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarLooking : MonoBehaviour
+{
+    public SpriteRenderer front;
+    public SpriteRenderer back;
+
+    public List<Sprite> frontTemplates;
+    public List<Sprite> backTemplates;
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    void Initialize()
+    {
+        int index = Random.Range(0, frontTemplates.Count);
+        front.sprite = frontTemplates[index];
+        back.sprite = backTemplates[index];
+    }
+}
