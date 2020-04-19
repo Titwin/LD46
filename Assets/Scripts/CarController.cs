c3e636e052e137caad1f91aa60ad0a0fed9cd4d9
Marco Benzi <marksolosw@gmail.com>
2020-04-19 16:33:05 +0200

    Added car door opening and closing, and engine sound SFXs

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -8,6 +8,12 @@ public class CarController : MonoBehaviour
     public Car car;
     
     public bool readInput = true;
+    private AudioListener audioListener;
+
+    void Start()
+    {
+        audioListener = GetComponent<AudioListener>();
+    }
 
     private void FixedUpdate()
     {
@@ -15,5 +21,28 @@ public class CarController : MonoBehaviour
         {
             car.SetInput(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
         }
+    }
+
+    public void StartEngine()
+    {
+        car.StartEngine();
+
+        if (audioListener)
+        {
+            Destroy(audioListener);
+            audioListener = null;
+        }
+        audioListener = gameObject.AddComponent<AudioListener>() as AudioListener;
+    }
+
+    public void StopEngine()
+    {
+        car.StopEngine();
+
+        if (audioListener)
+        {
+            Destroy(audioListener);
+            audioListener = null;
+        }            
     }
 }
